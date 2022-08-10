6a329c9735734525340cc1b89351fde5325e28b2
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-10 10:01:12 -0400

    commit tree 5b38ebfe644e0b838c85b34e9daab531d8619e92

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (drivers,fn) => fn(drivers);
